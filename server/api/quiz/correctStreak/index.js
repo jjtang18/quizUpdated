a9717200ef6b921d13943d5fb1559b09daa2098a
Joseph Tang <josephtang724@gmail.com>
2020-10-08 15:20:02 -0700

    a

--- server/api/quiz/correctStreak/index.js
+++ server/api/quiz/correctStreak/index.js
@@ -11,7 +11,7 @@ app.put('*', (req, res) => {
         throw new Error('No document id specified.');
       }
 
-      if (req.body === "true") {
+      if (req.body.toString() === "true") {
         return Question.findOneAndUpdate(
           { _id },
           {
