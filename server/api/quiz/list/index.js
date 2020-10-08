f5550c34c11cd57add28fe72f7db41b77ed68a92
Joseph Tang <josephtang724@gmail.com>
2020-10-07 20:41:19 -0700

    made additional modifications to the list/index.js  to follow syntax from tutorial

--- server/api/quiz/list/index.js
+++ server/api/quiz/list/index.js
@@ -5,16 +5,16 @@ const Question = require('../../../models/AddQuestion');
 app.get('*', (req, res) => {
   connectDB()
     .then(() => Question.find())
-    .then(questionItems => {
+    .then((questionItems) => {
       res.status(200).json({
-        result: questionItems,
-      });
+        result: questionItems
+      })
     })
     .catch(error => {
       res.status(error.statusCode || 500).json({
         error: error.message,
       });
-    });
+    })
 });
 
 module.exports = app;
